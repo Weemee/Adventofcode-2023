e212cea4954b72bb2e4d842b3aa8243679600b08
Chris Balder Andersson <chris.jager.sgs@gmail.com>
2023-12-07 20:33:26 +0100

    Day 6 completed

--- day_6.py
+++ day_6.py
@@ -1,11 +1,33 @@
-import re
+import math
 
 part1 = 0
 part2 = 0
 
-with open('day_6.txt', 'r') as t:
-    for c in t:
-        print(c.rstrip('\n'))
+data = open('day_6.txt').read().split('\n')
+sets = zip(list(filter(None, data[0].split(':')[1].split(' '))), list(filter(None, data[1].split(':')[1].split(' '))))
+
+# PQ-formeln https://www.matteboken.se/lektioner/matte-2/andragradsekvationer/pq-formeln#!/
+def quadratic(p, q):
+    x1 = math.floor((p / 2) + math.sqrt((p / 2) ** 2 - q))
+    x2 = math.ceil((p / 2) - math.sqrt((p / 2) ** 2 - q))
+
+    return x1 - x2 + 1
+    
+for set in sets:
+    time = int(set[0])
+    distance = int(set[1]) + 1
+
+    result = quadratic(time, distance)
+
+    if part1 == 0:
+        part1 = result
+    else:
+        part1 *= result
+
+t = int(''.join(list(filter(None, data[0].split(':')[1].split(' ')))))
+d = int(''.join(list(filter(None, data[1].split(':')[1].split(' ')))))
+
+part2 = quadratic(t, d)
 
 print('Part 1: ', part1)
 print('Part 2: ', part2)
